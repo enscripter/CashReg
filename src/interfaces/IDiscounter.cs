4dbbe8a6cd427a9f892863541b3d515986739250
izilla <>
2018-03-16 18:20:02 -0500

    An interface for something that discount

---
+++ src/interfaces/IDiscounter.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using CashReg.objects;
+
+namespace CashReg.interfaces
+{
+    /// <summary>
+    /// An interface for something that can discount a IEnumerable of ItemBases
+    /// </summary>
+    public interface IDiscounter
+    {
+        /// <summary>
+        /// Return the discount of this applied to all ItemBases in the IEnumerable
+        /// </summary>
+        /// <param name="items">An iterable object of ItemBases</param>
+        /// <returns>The total discount applied to all item</returns>
+        float Discout(IEnumerable<ItemBase> items);
+    }
+}
