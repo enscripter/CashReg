54c10c7f53f379f111ac70c055c4cc6c7246ffb2
izilla <>
2018-03-17 10:35:21 -0500

    A runnable interface and an interpreter for the command line

---
+++ src/interfaces/IRunnable.cs
@@ -0,0 +1,13 @@
+namespace CashReg.interfaces
+{
+    /// <summary>
+    /// A simple interface for all things that can run
+    /// </summary>
+    public interface IRunnable
+    {
+        /// <summary>
+        /// The run method to start this object
+        /// </summary>
+        void Run();
+    }
+}
---
+++ src/objects/CommandLineRegisterInterpreter.cs
@@ -0,0 +1,15 @@
+using CashReg.interfaces;
+
+namespace CashReg.objects
+{
+    public class CommandLineRegisterInterpreter : IRunnable
+    {
+        /// <summary>
+        /// This start the interpreter running and looking for input at the command line
+        /// </summary>
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
