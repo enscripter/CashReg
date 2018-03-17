20e9a5a37d4fabd2d51075444daa5c41cabf95df
izilla <>
2018-03-16 19:42:44 -0500

    Additional test

--- tests/ItemRegisterTests.cs
+++ tests/ItemRegisterTests.cs
@@ -10,9 +10,15 @@ namespace tests
     {
         private class FooItem : ItemBase
         {
+            private float val = 1.0F;
             public override float TotalValue()
             {
-                return 1.0F;
+                return val;
+            }
+            public override void Update(ItemBase item)
+            {
+                val += 1.0F;
+                return;
             }
         }
         [Fact]
@@ -22,9 +28,17 @@ namespace tests
             Assert.Equal(0, register.UniqueItemCount());
         }
         [Fact]
-        public void ItemRegister_Add_SingleItem()
+        public void ItemRegister_UniqueItemCount_SingleItem()
         {
             var register = new ItemRegister();
+            register.Add(new FooItem());
+            Assert.Equal(1, register.UniqueItemCount());
+        }
+        [Fact]
+        public void ItemRegister_UniqueItemCount_MultipleItems()
+        {
+            var register = new ItemRegister();
+            register.Add(new FooItem());
             register.Add(new FooItem());
             Assert.Equal(1, register.UniqueItemCount());
         }
@@ -36,12 +50,49 @@ namespace tests
             Assert.Equal(1.0, itemRegister.Total());
         }
         [Fact]
-        public void ItemRegister_Total_TenItem()
+        public void ItemRegister_Total_TenItems()
         {
             var itemRegister = new ItemRegister();
             for (int i = 0; i < 10; i++)
                 itemRegister.Add(new FooItem());
             Assert.Equal(10.0, itemRegister.Total());
+        }
+        [Fact]
+        public void ItemRegister_Add_WeightedItem()
+        {
+            var register = new ItemRegister();
+            register.Add(new WeightedItem(){
+                weight = 1,
+                value = 1
+            });
+            Assert.Equal(1, register.Total());
+        }
+        [Fact]
+        public void ItemRegister_Add_QuantityItem()
+        {
+            var register = new ItemRegister();
+            register.Add(new QuantityItem(){
+                quantity = 1,
+                value = 1
+            });
+            Assert.Equal(1, register.Total());
+        }
+        [Fact]
+        public void ItemRegister_Add_DifferentItems()
+        {
+            var register = new ItemRegister();
+            register.Add(new QuantityItem(){
+                name = "quantity",
+                quantity = 1,
+                value = 1
+            });
+            register.Add(new WeightedItem(){
+                name = "weighted",
+                weight = 1,
+                value = 1
+            });
+            register.Add(new FooItem());
+            Assert.Equal(3, register.Total());
         }
     }
 }
