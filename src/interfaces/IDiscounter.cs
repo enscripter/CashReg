b20473b3209dd59255b4ecc388545034465aabc4
izilla <>
2018-03-16 20:11:09 -0500

    Change the structure to be more generic

--- src/interfaces/IDiscounter.cs
+++ src/interfaces/IDiscounter.cs
@@ -6,13 +6,13 @@ namespace CashReg.interfaces
     /// <summary>
     /// An interface for something that can discount a IEnumerable of ItemBases
     /// </summary>
-    public interface IDiscounter
+    public interface IDiscounter<T>
     {
         /// <summary>
         /// Return the discount of this applied to all ItemBases in the IEnumerable
         /// </summary>
         /// <param name="items">An iterable object of ItemBases</param>
         /// <returns>The total discount applied to all item</returns>
-        float Discount(IEnumerable<ItemBase> items);
+        float Discount(IEnumerable<T> items);
     }
 }
