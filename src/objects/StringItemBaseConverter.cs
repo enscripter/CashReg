1c4bbfccd308206622444efdf90337356166ffd2
izilla <>
2018-03-16 22:09:27 -0500

    Correct name for file

---
+++ src/objects/StringItemBaseConverter.cs
@@ -0,0 +1,20 @@
+using CashReg.interfaces;
+
+namespace CashReg.objects
+{
+    /// <summary>
+    /// An implementation of a Converter from string to ItemBase
+    /// </summary>
+    public class StringItemBaseConverter : IConverter<string, ItemBase>
+    {
+        /// <summary>
+        /// Convert a string to a ItemBase
+        /// </summary>
+        /// <param name="input">The string to convert to an ItemBase</param>
+        /// <returns>An ItemBase instance based off the input string</returns>
+        public ItemBase Convert(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
